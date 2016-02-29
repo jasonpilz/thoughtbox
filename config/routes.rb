89b4a2e3c44cba02ef01b04aa0c03967255408c8
Jason Pilz <jasonpilz@gmail.com>
2016-02-29 10:06:57 -0700

    add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: 'links#index'
+
+  get '/login',  to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
 end
--- test/integration/user_sign_up_test.rb
+++ test/integration/user_sign_up_test.rb
@@ -4,6 +4,7 @@ class UserSignUpTest < ActionDispatch::IntegrationTest
   test "guest user gets redirected to login page" do
     visit root_path
 
+    assert_equal(login_path, current_path)
     assert page.has_content?("Sign Up")
   end
 end
