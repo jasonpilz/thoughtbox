f1006ef2aba3fa8833c82ac3a59951370df0a776
Jason Pilz <jasonpilz@gmail.com>
2016-02-29 17:13:00 -0700

    initial model tests passing

--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class LinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  valid_params = { title: "Turing today",
+                   url: "http://today.turing.io/outlines/2016-02-29/"}
+
+  should belong_to(:user)
+
+  test "it is valid with correct params" do
+    link = Link.new(valid_params)
+
+    assert link.valid?
+  end
+
+  test "should have a valid url" do
+    link = Link.new({ title: "hello",
+                      url: "notavalidurl" })
+
+    refute link.valid?
+  end
 end
